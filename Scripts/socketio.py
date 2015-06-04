a434fec298d8b7bf02d65ce00b70f492f8970080
Matthijs <matthijsthoolen@hotmail.com>
2015-06-04 20:15:48 +0200

    Websockets basics are working now

--- Scripts/socketio.py
+++ Scripts/socketio.py
@@ -1,5 +1,61 @@
-import socket
+# from socketIO_client import SocketIO
+# with SocketIO('localhost', 8080) as socketIO:
+# 	socketIO.emit('aaa')
+# 	socketIO.wait(seconds=1)
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8080))
-clientsocket.send('hello')
+import websocket
+import thread
+import time
+import sys
+from urllib import *
+ 
+class SocketIO:
+	def __init__(self):
+		self.PORT = 8080
+		self.HOSTNAME = 'localhost'
+		self.connect()
+ 
+	def __del__(self):
+		self.close()
+ 
+	def handshake(self,host,port):
+# 		u = urlopen("http://%s:%d/socket.io/1" % (host, port))
+# 		print u.getcode()
+# 		if u.getcode() == 200:
+# 			response = u.readline()
+# 			print response
+# 			(sid, hbtimeout, ctimeout, supported) = response.split(":")
+# 			supportedlist = supported.split(",")
+# 			if "websocket" in supportedlist:
+# 				return (sid, hbtimeout, ctimeout)
+# 			else:
+# 				raise TransportException()
+# 		else:
+# 			raise InvalidResponseException()
+		print 'haha'
+		return ('10', '10', '10')
+ 
+	def connect(self):
+		try:
+			(sid, hbtimeout, ctimeout) = self.handshake(self.HOSTNAME, self.PORT) #handshaking according to socket.io spec.
+			self.ws = websocket.create_connection("ws://%s:%d/socket.io/1/websocket/%s" % (self.HOSTNAME, self.PORT, sid))
+		except Exception as e:
+			print e
+			sys.exit(1)
+ 
+	def heartbeat(self):
+		self.ws.send("2::")
+ 
+	def send(self,event,message):
+		self.heartbeat()
+		self.ws.send('5:1::{"name":"%s","args":"%s"}' % (event, message))
+ 
+	def close(self):
+		print "closihg!"
+		self.ws.close()
+ 
+if __name__ == "__main__":
+	print "hello"
+	s = SocketIO()
+	s.send("pyevent", "message")
+	s.send("pyevent", "message2")
--- Site/socket.js
+++ Site/socket.js
@@ -1,17 +1,75 @@
-var io = require('socket.io')(8080);
+var ws = require("nodejs-websocket");
 
-console.log('Server started!');
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection");
+    conn.on("text", function (str) {
+        console.log("Received "+str);
+        conn.sendText(str.toUpperCase()+"!!!");
+    });
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed");
+    });
+}).listen(8080);
 
-io.on('connection', function(socket) {
-	io.emit('this', {will: 'be received by everyone'});
-	
-	console.log('Hello There!');
-	
-	socket.on('private message', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
+/* var ws = require('websocket.io')
+  , server = ws.listen(8080)
+ 
+server.on('connection', function (client) {
+  client.on('message', function () { });
+  client.on('close', function () { });
+}); */
 
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
-  });
+/* #!/usr/bin/env node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(200);
+	response.write("500:10:10:websocket");
+    response.end();
 });
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: true
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+}); */
