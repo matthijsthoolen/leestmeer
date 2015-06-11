59f7bff196e2d1bef293e784e2893dcb32de0c86
woofwoofus <woofwoofus@gmail.com>
2015-06-11 16:11:40 +0200

    N-Gram analysis of corpus

--- Scripts/readDatabaseJan.py
+++ Scripts/readDatabaseJan.py
@@ -1,4 +1,6 @@
 import sys, getopt, argparse, re, pickle, urllib.parse
+from html.parser import HTMLParser as HP
+import html
 import xml.etree.ElementTree as ET
 import AVIscoreMod
 
@@ -15,20 +17,38 @@ def main(corpus):
 
 	# Here the empty variables and their types should be defined
 	# e.g.: domains = []
-	domains = []
+	# domains = []
 
 	# Body of the function
-	file = open('articles.txt','w')
+	# aviAgeDict = {'0':'Adult', '1':'MaartGroep3', '2':'EindGroep3', '3':'NovGroep4', '4':'MaartGroep4', '5':'EindGroep4', '6':'NovGroep5', '7':'MaartGroep5', '8':'EindGroep5', '9':'KerstGroep6'}
+	
 	for article in content:
 		loc = urllib.parse.urlparse(article['identifier']).netloc
-		if domains.count(loc) == 0:
-			domains.append(loc)
-		# print(article['body'])
-		bod = remove_tags(article['body'])
-		file.write('AVI score: ' + str(AVIscoreMod.main(bod,'avi','text')))
-		file.write('\n')
-		file.write(bod)
-		file.write('\n ----------------------- \n')
+		# bod = HP.feed(article['body'])
+		body = remove_tags(html.unescape(article['body']))
+		bod = re.split('\.[\s|\n]|\!+|\?+',body)
+		# body = remove_tags(article['body'])
+		# bod = bytes(body, 'utf-8')
+		# print(bytes(body,'utf-8'))
+
+		# bod = remove_tags(article['body'])
+		# print(type(bod))
+
+		if loc == 'm.bright.nl':
+			loc = 'www.bright.nl'
+			# bod = bytes(body, 'utf-8')
+		elif loc == 'nos.nl':
+			loc = 'www.nos.nl'
+			# bod = bytes(body, 'utf-8')
+		try:
+			file = open('database\\' + loc,'a')
+		except IOError:
+			file = open('database\\' + loc,'w')
+		for line in bod:
+			file.write(line + '\n\n')
+		file.close()
+	# print(domains)
+
 
 def remove_tags(text):
 	TAG_RE = re.compile(r'<[^>]+>')
